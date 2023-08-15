fcf9453cdea2e047ffbd184eddc5da88bde63f58
onee <onee.me@outlook.com>
2023-08-15 13:08:45 +0800

    fix plane and wold demo

--- WhatVisionOSCanDo/Item/PlaneClassification/PlaneClassificationImmersiveView.swift
+++ WhatVisionOSCanDo/Item/PlaneClassification/PlaneClassificationImmersiveView.swift
@@ -12,10 +12,7 @@ struct PlaneClassificationImmersiveView: View {
     
     var body: some View {
         RealityView { content in
-            // Add the initial RealityKit content
-            if let scene = try? await Entity(named: "Immersive", in: realityKitContentBundle) {
-                content.add(scene)
-            }
+            content.add(model.rootEntity)
         }
         .task {
             await model.run(
--- WhatVisionOSCanDo/Item/WorldScening/WorldSceningImmersiveView.swift
+++ WhatVisionOSCanDo/Item/WorldScening/WorldSceningImmersiveView.swift
@@ -15,10 +15,7 @@ struct WorldSceningImmersiveView: View {
 
     var body: some View {
         RealityView { content in
-            // Add the initial RealityKit content
-            if let scene = try? await Entity(named: "Immersive", in: realityKitContentBundle) {
-                content.add(scene)
-            }
+            content.add(model.rootEntity)
         }
         .task {
             await model.run(
