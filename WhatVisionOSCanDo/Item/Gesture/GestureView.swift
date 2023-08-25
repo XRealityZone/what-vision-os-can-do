a79352313c821e5814c7fc4df17bcd111b8d8858
onee <onee.me@outlook.com>
2023-08-25 23:01:02 +0800

    add basic gesture toggle

--- WhatVisionOSCanDo/Item/Gesture/GestureView.swift
+++ WhatVisionOSCanDo/Item/Gesture/GestureView.swift
@@ -9,7 +9,13 @@ import RealityKit
 import RealityKitContent
 import SwiftUI
 
+enum GestureToggles {
+    case null, tap, drag, longPress, magnify, rotate, rotate3D
+}
+
 struct GestureView: View {
+    @State private var enabledGesture: GestureToggles = .tap
+
     var body: some View {
         VStack {
             Text("Hello World")
@@ -20,23 +26,63 @@ struct GestureView: View {
                 } catch {
                     print("load entity error, error is \(error)")
                 }
-            } update: { _ in
             }
-            .gesture(MagnifyGesture().targetedToAnyEntity().onEnded({ action in
-                print("MagnifyGesture ended")
-            }))
-            .gesture(TapGesture().targetedToAnyEntity().onEnded({ action in
+            HStack {
+                Toggle("Tap", isOn: Binding(get: {
+                    enabledGesture == .tap
+                }, set: { value in
+                    enabledGesture = value ? .tap : .null
+                }))
+                .toggleStyle(.button)
+                Toggle("Drag", isOn: Binding(get: {
+                    enabledGesture == .drag
+                }, set: { value in
+                    enabledGesture = value ? .drag : .null
+                }))
+                .toggleStyle(.button)
+                Toggle("LongPress", isOn: Binding(get: {
+                    enabledGesture == .longPress
+                }, set: { value in
+                    enabledGesture = value ? .longPress : .null
+                }))
+                .toggleStyle(.button)
+                Toggle("Magnify", isOn: Binding(get: {
+                    enabledGesture == .magnify
+                }, set: { value in
+                    enabledGesture = value ? .magnify : .null
+                }))
+                .toggleStyle(.button)
+                Toggle("Rotate", isOn: Binding(get: {
+                    enabledGesture == .rotate
+                }, set: { value in
+                    enabledGesture = value ? .rotate : .null
+                }))
+                .toggleStyle(.button)
+                Toggle("Rotate3D", isOn: Binding(get: {
+                    enabledGesture == .rotate3D
+                }, set: { value in
+                    enabledGesture = value ? .rotate3D : .null
+                }))
+                .toggleStyle(.button)
+            }
+            .gesture(enabledGesture == .tap ? TapGesture().targetedToAnyEntity().onEnded { _ in
                 print("TapGesture ended")
-            }))
-            .gesture(RotateGesture().targetedToAnyEntity().onEnded({ action in
-                print("RotateGesture ended")
-            }))
-            .gesture(LongPressGesture().targetedToAnyEntity().onEnded({ action in
-                print("LongPressGesture ended")
-            }))
-            .gesture(DragGesture().targetedToAnyEntity().onEnded({ action in
+            } : nil)
+            .gesture(enabledGesture == .drag ?DragGesture().targetedToAnyEntity().onEnded { _ in
                 print("DragGesture ended")
-            }))
+            } : nil)
+            .gesture(enabledGesture == .longPress ?LongPressGesture().targetedToAnyEntity().onEnded { _ in
+                print("LongPressGesture ended")
+            } : nil)
+            .gesture(enabledGesture == .magnify ? MagnifyGesture().targetedToAnyEntity().onEnded { _ in
+                print("MagnifyGesture ended")
+            } : nil)
+            .gesture(enabledGesture == .rotate ?RotateGesture().targetedToAnyEntity().onEnded { _ in
+                print("RotateGesture ended")
+            } : nil)
+            .gesture(enabledGesture == .rotate3D ?RotateGesture3D(constrainedToAxis: .x).targetedToAnyEntity().onEnded { _ in
+                print("RotateGesture3D ended")
+            } : nil)
         }
     }
 }
